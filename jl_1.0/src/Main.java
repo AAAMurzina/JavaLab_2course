0e2ce41f6ee7e53a946dc58903c554cc1fe4eb1f
AAAMurzina <ANMurzina@stud.kpfu.ru>
2019-09-30 13:55:21 +0300

    added homework

--- jl_1.0/src/Main.java
+++ jl_1.0/src/Main.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Scanner sc =  new Scanner(System.in);
-        String url = sc.nextLine();
-        String[] ArrUrl = url.split(" ");
+        String[] ArrUrl = args;
         UrlThread[] tread = new UrlThread[ArrUrl.length];
         for (int i = 0; i < ArrUrl.length; i++) {
             tread[i] = new UrlThread(ArrUrl[i]);
